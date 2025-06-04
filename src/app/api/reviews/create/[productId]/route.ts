34bc45a3a1b11b4d1b3b99ebd80a47ff9259c534
BnjmnCummings <cummingsben097@gmail.com>
2025-06-04 23:45:43 +0100

    fix: removed username field from createReview end point

--- src/app/api/reviews/create/[productId]/route.ts
+++ src/app/api/reviews/create/[productId]/route.ts
@@ -8,13 +8,12 @@ export async function POST(req: NextRequest, { params }: { params: Promise<{ pro
         const { searchParams } = new URL(req.url);
 
         const userId = searchParams.get('userId');
-        const username = searchParams.get('username');
         const reviewText = searchParams.get('reviewText') || undefined;
         const valueRating = parseInt(searchParams.get('valueRating'));
         const qualityRating = parseInt(searchParams.get('qualityRating'));
         
-        if (!userId || !username) {
-            return NextResponse.json({ success: false, error: 'Missing UserId or Username' }, { status: 400 });
+        if (!userId) {
+            return NextResponse.json({ success: false, error: 'Missing UserId' }, { status: 400 });
         }
         const review: Omit<Review, 'id'> & { createdAt: Date, productId: string } = {
             createdAt: new Date(),
